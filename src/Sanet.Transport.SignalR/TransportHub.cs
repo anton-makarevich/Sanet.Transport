070af52a22c64f753782b7166e7f205b9f819f53
Anton Makarevich <anton.makarevich@gmail.com>
2025-03-27 22:37:48 +0100

    feat: add signalr project

---
+++ src/Sanet.Transport.SignalR/TransportHub.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Sanet.Transport.SignalR;
+
+/// <summary>
+/// SignalR Hub for transport message communication
+/// </summary>
+public class TransportHub : Hub
+{
+    /// <summary>
+    /// Event that fires when a message is received from a client
+    /// </summary>
+    public static event Action<TransportMessage>? OnMessageReceived;
+    
+    /// <summary>
+    /// Receives a message from a client and broadcasts it to all connected clients
+    /// </summary>
+    /// <param name="message">The transport message to broadcast</param>
+    /// <returns>A task representing the asynchronous operation</returns>
+    public async Task SendMessage(TransportMessage message)
+    {
+        // Invoke the event to notify local subscribers
+        OnMessageReceived?.Invoke(message);
+        
+        // Broadcast the message to all connected clients
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
